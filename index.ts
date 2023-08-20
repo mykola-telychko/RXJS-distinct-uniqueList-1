d650ad9c012a0e34904316a958e574b4014b025c
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-20 23:18:43 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ console.clear();
 import { of } from 'rxjs';
 import { distinct } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/filtering/distinct
+
 const nonUniqueList$ = of(1, 2, 3, 4, 5, 1, 2, 3, 4, 5);
 
 nonUniqueList$.pipe(distinct()).subscribe(console.log);
